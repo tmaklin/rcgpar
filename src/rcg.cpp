f9ba8aeb9953c60b75d0b15111867f8193ebdfdb
Tommi Mäklin <tommi@maklin.fi>
2021-11-03 16:41:19 +0200

    Use explicit (unsigned) integers.

--- src/rcg.cpp
+++ src/rcg.cpp
@@ -28,44 +28,44 @@ double digamma(double x) {
 }
 
 void logsumexp(Matrix<double> &gamma_Z) {
-    unsigned n_cols = gamma_Z.get_cols();
-    unsigned short n_rows = gamma_Z.get_rows();
+    uint32_t n_cols = gamma_Z.get_cols();
+    uint16_t n_rows = gamma_Z.get_rows();
 
 #pragma omp parallel for schedule(static)
-    for (unsigned i = 0; i < n_cols; ++i) {
+    for (uint32_t i = 0; i < n_cols; ++i) {
 	double m = gamma_Z.log_sum_exp_col(i);
-	for (short unsigned j = 0; j < n_rows; ++j) {
+	for (uint16_t j = 0; j < n_rows; ++j) {
 	    gamma_Z(j, i) -= m;
 	}
     }
 }
 
 void logsumexp(Matrix<double> &gamma_Z, std::vector<double> &m) {
-    unsigned n_cols = gamma_Z.get_cols();
-    unsigned short n_rows = gamma_Z.get_rows();
+    uint32_t n_cols = gamma_Z.get_cols();
+    uint16_t n_rows = gamma_Z.get_rows();
 
 #pragma omp parallel for schedule(static)
-    for (unsigned i = 0; i < n_cols; ++i) {
+    for (uint32_t i = 0; i < n_cols; ++i) {
 	m[i] = gamma_Z.log_sum_exp_col(i);
     }
 
 #pragma omp parallel for schedule(static)
-    for (unsigned short i = 0; i < n_rows; ++i) {
-	for (unsigned j = 0; j < n_cols; ++j) {
+    for (uint16_t i = 0; i < n_rows; ++i) {
+	for (uint32_t j = 0; j < n_cols; ++j) {
 	    gamma_Z(i, j) -= m[j];
 	}
     }
 }
 
 double mixt_negnatgrad(const Matrix<double> &gamma_Z, const std::vector<double> &N_k, const Matrix<double> &logl, Matrix<double> &dL_dphi) {
-    unsigned n_cols = gamma_Z.get_cols();
-    unsigned short n_rows = gamma_Z.get_rows();
+    uint32_t n_cols = gamma_Z.get_cols();
+    uint16_t n_rows = gamma_Z.get_rows();
 
     std::vector<double> colsums(n_cols, 0.0);
 #pragma omp parallel for schedule(static) reduction(vec_double_plus:colsums)
-    for (unsigned short i = 0; i < n_rows; ++i) {
+    for (uint16_t i = 0; i < n_rows; ++i) {
 	double digamma_N_k = digamma(N_k[i]) - 1.0;
-	for (unsigned j = 0; j < n_cols; ++j) {
+	for (uint32_t j = 0; j < n_cols; ++j) {
 	    dL_dphi(i, j) = logl(i, j);
 	    dL_dphi(i, j) += digamma_N_k - gamma_Z(i, j);
 	    colsums[j] += dL_dphi(i, j) * std::exp(gamma_Z(i, j));
@@ -74,8 +74,8 @@ double mixt_negnatgrad(const Matrix<double> &gamma_Z, const std::vector<double> 
   
     double newnorm = 0.0;
 #pragma omp parallel for schedule(static) reduction(+:newnorm)
-    for (unsigned short i = 0; i < n_rows; ++i) {
-	for (unsigned j = 0; j < n_cols; ++j) {
+    for (uint16_t i = 0; i < n_rows; ++i) {
+	for (uint32_t j = 0; j < n_cols; ++j) {
 	    // dL_dgamma(i, j) would be q_Z(i, j) * (dL_dphi(i, j) - colsums[j])
 	    newnorm += std::exp(gamma_Z(i, j)) * (dL_dphi(i, j) - colsums[j]) * dL_dphi(i, j);
 	}
@@ -84,11 +84,11 @@ double mixt_negnatgrad(const Matrix<double> &gamma_Z, const std::vector<double> 
 }
 
 void ELBO_rcg_mat(const Matrix<double> &logl, const Matrix<double> &gamma_Z, const std::vector<double> &counts, const std::vector<double> &alpha0, const std::vector<double> &N_k, long double &bound) {
-    unsigned short n_rows = gamma_Z.get_rows();
-    unsigned n_cols = gamma_Z.get_cols();
+    uint16_t n_rows = gamma_Z.get_rows();
+    uint32_t n_cols = gamma_Z.get_cols();
 #pragma omp parallel for schedule(static) reduction(+:bound)
-    for (unsigned short i = 0; i < n_rows; ++i) {
-	for (unsigned j = 0; j < n_cols; ++j) {
+    for (uint16_t i = 0; i < n_rows; ++i) {
+	for (uint32_t j = 0; j < n_cols; ++j) {
 	    bound += std::exp(gamma_Z(i, j) + counts[j])*(logl(i, j) - gamma_Z(i, j));
 	}
 	bound -= std::lgamma(alpha0[i]) - std::lgamma(N_k[i]);
@@ -96,11 +96,11 @@ void ELBO_rcg_mat(const Matrix<double> &logl, const Matrix<double> &gamma_Z, con
 }
 
 void revert_step(Matrix<double> &gamma_Z, const std::vector<double> &oldm) {
-    short unsigned n_rows = gamma_Z.get_rows();
-    unsigned n_cols = gamma_Z.get_cols();
+    uint16_t n_rows = gamma_Z.get_rows();
+    uint32_t n_cols = gamma_Z.get_cols();
 #pragma omp parallel for schedule(static)
-    for (short unsigned i = 0; i < n_rows; ++i) {
-	for (unsigned j = 0; j < n_cols; ++j) {
+    for (uint16_t i = 0; i < n_rows; ++i) {
+	for (uint32_t j = 0; j < n_cols; ++j) {
 	    gamma_Z(i, j) += oldm[j];
 	}
     }
@@ -124,7 +124,7 @@ Matrix<double> rcg_optl_mat(const Matrix<double> &logl, const std::vector<double
     }
   
 #pragma omp parallel for schedule(static) reduction(+:bound_const)
-    for (unsigned short i = 0; i < n_rows; ++i) {
+    for (uint16_t i = 0; i < n_rows; ++i) {
 	bound_const += alpha0[i];
 	bound_const += std::lgamma(alpha0[i]);
     }
@@ -134,7 +134,7 @@ Matrix<double> rcg_optl_mat(const Matrix<double> &logl, const std::vector<double
     gamma_Z.exp_right_multiply(log_times_observed, N_k);
   
 #pragma omp parallel for schedule(static)
-    for (unsigned short i = 0; i < n_rows; ++i) {
+    for (uint16_t i = 0; i < n_rows; ++i) {
 	N_k[i] += alpha0[i];
     }
   
@@ -156,7 +156,7 @@ Matrix<double> rcg_optl_mat(const Matrix<double> &logl, const std::vector<double
 	gamma_Z.exp_right_multiply(log_times_observed, N_k);
 
 #pragma omp parallel for schedule(static)
-	for (unsigned short i = 0; i < n_rows; ++i) {
+	for (uint16_t i = 0; i < n_rows; ++i) {
 	    N_k[i] += alpha0[i];
 	}
     
@@ -174,7 +174,7 @@ Matrix<double> rcg_optl_mat(const Matrix<double> &logl, const std::vector<double
 	    gamma_Z.exp_right_multiply(log_times_observed, N_k);
       
 #pragma omp parallel for schedule(static)
-	    for (unsigned short i = 0; i < n_rows; ++i) {
+	    for (uint16_t i = 0; i < n_rows; ++i) {
 		N_k[i] += alpha0[i];
 	    }
 
