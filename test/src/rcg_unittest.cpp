4c629972cb2a8597864aedb78b89163a973def23
Tommi Mäklin <tommi@maklin.fi>
2021-11-08 18:34:37 +0200

    Add test for rcg.cpp::digamma.

--- test/src/rcg_unittest.cpp
+++ test/src/rcg_unittest.cpp
@@ -18,34 +18,14 @@
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
 // USA
 //
-#ifndef RCGPAR_RCG_HPP
-#define RCGPAR_RCG_HPP
+#include "gtest/gtest.h"
 
-#include <vector>
+#include "rcg.hpp"
+#include "openmp_config.hpp"
 
-#include "Matrix.hpp"
+TEST(rcg, digammaWorks) {
+    double expected = -2.035671;
+    double got = rcgpar::digamma(0.485734);
 
-namespace rcgpar {
-double digamma(double in);
-void logsumexp(Matrix<double> &gamma_Z);
-void logsumexp(Matrix<double> &gamma_Z, std::vector<double> &m);
-
-double mixt_negnatgrad(const Matrix<double> &gamma_Z,
-		       const std::vector<double> &N_k,
-		       const Matrix<double> &logl, Matrix<double> &dL_dphi);
-
-void ELBO_rcg_mat(const Matrix<double> &logl, const Matrix<double> &gamma_Z,
-		  const std::vector<double> &counts,
-		  const std::vector<double> &alpha0,
-		  const std::vector<double> &N_k, long double &bound);
-
-void revert_step(Matrix<double> &gamma_Z, const std::vector<double> &oldm);
-
-double calc_bound_const(const std::vector<double> &log_times_observed,
-			const std::vector<double> &alpha0);
-
-void add_alpha0_to_Nk(const std::vector<double> &alpha0,
-		      std::vector<double> &N_k);
+    EXPECT_NEAR(expected, got, 1e-7);
 }
-
-#endif
