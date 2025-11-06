efe68300d7951e0c442e65df7e7c322ab01c7e4b
Tommi Mäklin <tommi@maklin.fi>
2025-11-06 19:14:45 +0100

    optimize() with 32bit or 64bit float precision

--- src/lib.rs
+++ src/lib.rs
@@ -35,6 +35,7 @@ pub fn optimize<F: Float + FromPrimitive, U: PrimInt>(
     log_likelihood: &[Vec<F>],
     counts: &[U],
     prior: &[F],
+    use_f32: bool,
 ) -> Result<Vec<F>, E> {
     assert_eq!(log_likelihood[0].len(), counts.len());
     assert_eq!(log_likelihood.len(), prior.len());
@@ -42,21 +43,40 @@ pub fn optimize<F: Float + FromPrimitive, U: PrimInt>(
     let n_rows = log_likelihood[0].len();
     let n_cols = log_likelihood.len();
 
-    let logl_floats = log_likelihood.iter().flat_map(|x| x.iter().map(|y| y.to_f32().unwrap()).collect::<Vec<f32>>()).collect::<Vec<f32>>();
-    let log_counts_floats = counts.iter().map(|x| x.to_f32().unwrap().ln()).collect::<Vec<f32>>();
-    let alpha0_floats = prior.iter().map(|x| x.to_f32().unwrap()).collect::<Vec<f32>>();
+    // TODO Figure out a cleaner way to write selecting f32 vs. f64 precision.
+    let proportions = if use_f32 {
+        let logl_floats = log_likelihood.iter().flat_map(|x| x.iter().map(|y| y.to_f32().unwrap()).collect::<Vec<f32>>()).collect::<Vec<f32>>();
+        let log_counts_floats = counts.iter().map(|x| x.to_f32().unwrap().ln()).collect::<Vec<f32>>();
+        let alpha0_floats = prior.iter().map(|x| x.to_f32().unwrap()).collect::<Vec<f32>>();
 
-    let device = Default::default();
-    type Backend = NdArray<f32>;
+        let device = Default::default();
+        type Backend = NdArray<f32>;
 
-    let logl_flat = Tensor::<Backend, 1>::from_data(logl_floats.as_slice(), &device);
-    let logl = logl_flat.reshape([n_cols, n_rows]);
+        let logl_flat = Tensor::<Backend, 1>::from_data(logl_floats.as_slice(), &device);
+        let logl = logl_flat.reshape([n_cols, n_rows]);
 
-    let log_counts = Tensor::<Backend, 1>::from_data(log_counts_floats.as_slice(), &device);
-    let alpha0 = Tensor::<Backend, 1>::from_data(alpha0_floats.as_slice(), &device);
+        let log_counts = Tensor::<Backend, 1>::from_data(log_counts_floats.as_slice(), &device);
+        let alpha0 = Tensor::<Backend, 1>::from_data(alpha0_floats.as_slice(), &device);
 
-    let probs = rcg::rcg_optl_mat(logl, log_counts.clone(), alpha0)?;
-    let proportions = rcg::mixture_components(probs, log_counts)?.into_data().iter().map(|x| FromPrimitive::from_f32(x).unwrap()).collect::<Vec<F>>();
+        let probs = rcg::rcg_optl_mat(logl, log_counts.clone(), alpha0)?;
+        rcg::mixture_components(probs, log_counts)?.into_data().iter().map(|x| FromPrimitive::from_f32(x).unwrap()).collect::<Vec<F>>()
+    } else {
+        let logl_floats = log_likelihood.iter().flat_map(|x| x.iter().map(|y| y.to_f64().unwrap()).collect::<Vec<f64>>()).collect::<Vec<f64>>();
+        let log_counts_floats = counts.iter().map(|x| x.to_f64().unwrap().ln()).collect::<Vec<f64>>();
+        let alpha0_floats = prior.iter().map(|x| x.to_f64().unwrap()).collect::<Vec<f64>>();
+
+        let device = Default::default();
+        type Backend = NdArray<f64>;
+
+        let logl_flat = Tensor::<Backend, 1>::from_data(logl_floats.as_slice(), &device);
+        let logl = logl_flat.reshape([n_cols, n_rows]);
+
+        let log_counts = Tensor::<Backend, 1>::from_data(log_counts_floats.as_slice(), &device);
+        let alpha0 = Tensor::<Backend, 1>::from_data(alpha0_floats.as_slice(), &device);
+
+        let probs = rcg::rcg_optl_mat(logl, log_counts.clone(), alpha0)?;
+        rcg::mixture_components(probs, log_counts)?.into_data().iter().map(|x| FromPrimitive::from_f64(x).unwrap()).collect::<Vec<F>>()
+    };
 
     Ok(proportions)
 }
@@ -67,7 +87,7 @@ mod tests {
     use assert_approx_eq::assert_approx_eq;
 
     #[test]
-    fn optimize() {
+    fn optimize_f64() {
         use burn::backend::ndarray::NdArray;
         use burn_tensor::backend::Device;
         use burn_tensor::Tensor;
@@ -85,9 +105,34 @@ mod tests {
         let counts: Vec<u32> = vec![2167, 1145, 943, 196, 175, 158, 1041, 957, 1447, 2135];
         let prior_counts: Vec<f64> = vec![1.0, 1.0, 1.0, 1.0];
 
-        let expected: Vec<f64> = vec![0.999543, 0.00073079, 9.66135e-05, 0.000112505];
-        let got = optimize(&log_likelihood, &counts, &prior_counts).unwrap();
+        let expected: Vec<f64> = vec![0.9990609232614853, 0.0007300889486079688, 9.656361438673255e-5, 0.00011242417552052694];
+        let got = optimize(&log_likelihood, &counts, &prior_counts, false).unwrap();
 
-        got.iter().zip(expected.iter()).for_each(|(x, y)| { assert_approx_eq!(x, y, 1e-2) });
+        got.iter().zip(expected.iter()).for_each(|(x, y)| { assert_approx_eq!(x, y, 1e-64) });
+    }
+
+    #[test]
+    fn optimize_f32() {
+        use burn::backend::ndarray::NdArray;
+        use burn_tensor::backend::Device;
+        use burn_tensor::Tensor;
+        use burn_tensor::Int;
+
+        use super::optimize;
+
+        let log_likelihood: Vec<Vec<f32>> =
+            vec![
+                vec![ -0.0100503, -0.0100503, -0.0100503, -0.0100503, -0.0100503, -0.0100503, -0.0100503, -0.0100503, -0.0100503, -0.0100503 ],
+                vec![ -0.0100503, -0.0100503, -0.0100503, -0.0100503, -0.0100503, -0.0100503, -0.0100503, -0.0100503, -0.0100503, -0.371713 ],
+                vec![ -0.0100503, -0.0100503, -0.0100503, -0.371713,  -0.371713,  -0.371713,  -4.60517,   -4.60517,   -4.60517,   -0.0100503 ],
+                vec![ -0.0100503, -0.371713,  -4.60517,   -0.0100503, -0.371713,  -4.60517,   -0.0100503, -0.371713,  -4.60517,   -0.0100503 ],
+            ];
+        let counts: Vec<u32> = vec![2167, 1145, 943, 196, 175, 158, 1041, 957, 1447, 2135];
+        let prior_counts: Vec<f32> = vec![1.0, 1.0, 1.0, 1.0];
+
+        let expected: Vec<f32> = vec![0.9990609232614853, 0.0007300889486079688, 9.656361438673255e-5, 0.00011242417552052694];
+        let got = optimize(&log_likelihood, &counts, &prior_counts, false).unwrap();
+
+        got.iter().zip(expected.iter()).for_each(|(x, y)| { assert_approx_eq!(x, y, 1e-9) });
     }
 }
